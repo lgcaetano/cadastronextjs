a92e7c517c7639a8c836fa4176cec9f2f6f3ced9
Luis Gabriel <luisgabrielcaetano@gmail.com>
2021-10-18 23:55:22 -0300

    fixing some styling bugs that happened when generating the production build

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,10 +8,16 @@ interface ButtonProps {
 
 export default function Button(props: ButtonProps){
 
+    const gradients = {
+        green: 'from-green-500 to-green-700',
+        blue: 'from-blue-500 to-blue-700',
+        gray: 'from-gray-500 to-gray-700',
+    }
+
     const cor = props.cor ?? 'gray' 
 
     return (
-        <button className={` bg-gradient-to-r from-${cor}-500 to-${cor}-700
+        <button className={` bg-gradient-to-r ${gradients[cor]}
          rounded-md py-2 px-4 m-2 mt-0 text-gray-100 font-bold self-end`} onClick={props.onClickFunction}>
             {props.children}
         </button>
