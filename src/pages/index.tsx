82898ba594273b7544d70cdbc65927a8b23ddcec
Luis Gabriel <luisgabrielcaetano@gmail.com>
2021-10-18 23:41:39 -0300

    testing something for bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,6 @@ export default function Home() {
      text-white
     `}>
       <Layout title="Cadastro Simples">
-        <h1>{process.env.NEXT_PUBLIC_FIREBASE_API_KEY}</h1>
         {formMode ? formComponent : cadastroComponent}
       </Layout>
     </div>
